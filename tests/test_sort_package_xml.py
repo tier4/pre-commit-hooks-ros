d0c2bb29f5f6d60e071384390334d02c6ec69b1b
Takagi, Isamu <isamu.takagi@tier4.jp>
2022-02-23 00:41:12 +0900

    fix: test for sort-pakcage-xml

--- tests/test_sort_package_xml.py
+++ tests/test_sort_package_xml.py
@@ -3,16 +3,18 @@ import pytest
 from pre_commit_hooks import sort_package_xml
 
 cases = [
-  ('tests/resources/rospkg1/package.xml', 'tests/resources/rospkg3/package.xml', 1),
-  ('tests/resources/rospkg2/package.xml', 'tests/resources/rospkg3/package.xml', 0),
+  ('package.ok.xml', 'package.ans.xml', 0),
+  ('package.ng.xml', 'package.ans.xml', 1),
 ]
 
 @pytest.mark.parametrize(('target_file', 'answer_file', 'answer_code'), cases)
-def test(target_file, answer_file, answer_code):
+def test(target_file, answer_file, answer_code, datadir):
 
-    return_code = sort_package_xml.main([target_file])
-    target_text = pathlib.Path(target_file).read_text()
-    answer_text = pathlib.Path(answer_file).read_text()
+  target_path = datadir.joinpath(target_file)
+  answer_path = datadir.joinpath(answer_file)
+  return_code = sort_package_xml.main([str(target_path)])
+  target_text = pathlib.Path(target_path).read_text()
+  answer_text = pathlib.Path(answer_path).read_text()
 
-    assert return_code == answer_code
-    assert target_text == answer_text
+  assert return_code == answer_code
+  assert target_text == answer_text
