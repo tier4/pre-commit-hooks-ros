910f1d6c19ff50909beafa528fd50ccbcfb45d86
Takagi, Isamu <isamu.takagi@tier4.jp>
2022-02-23 04:40:51 +0900

    fix: pre-commit

--- pre_commit_hooks/ros_include_guard.py
+++ pre_commit_hooks/ros_include_guard.py
@@ -17,152 +17,149 @@ import pathlib
 
 
 class DirectiveBase:
+    def __init__(self, directive):
+        self.directive = directive
+        self.line = None
+        self.text = None
+        self.expected = None
+        self.tokens = None
 
-  def __init__(self, directive):
-    self.directive = directive
-    self.line = None
-    self.text = None
-    self.expected = None
-    self.tokens = None
+    def update(self, line, text):
+        self.line = line
+        self.text = text
 
-  def update(self, line, text):
-    self.line = line
-    self.text = text
+    def is_none(self):
+        return self.line is None
 
-  def is_none(self):
-    return self.line is None
+    def mismatch(self):
+        return self.text != self.expected
 
-  def mismatch(self):
-    return self.text != self.expected
-
-  def overwrite(self, lines):
-    lines[self.line] = self.expected
+    def overwrite(self, lines):
+        lines[self.line] = self.expected
 
 
 class OpeningDirective(DirectiveBase):
-
-  def prepare(self, macro_name):
-    self.tokens = split_space_boundary(self.text)
-    if 3 <= len(self.tokens):
-      self.tokens[2] = macro_name
-      self.expected = ''.join(self.tokens)
-    else:
-      self.expected = F'{self.directive} {macro_name}'
+    def prepare(self, macro_name):
+        self.tokens = split_space_boundary(self.text)
+        if 3 <= len(self.tokens):
+            self.tokens[2] = macro_name
+            self.expected = "".join(self.tokens)
+        else:
+            self.expected = f"{self.directive} {macro_name}"
 
 
 class ClosingDirective(DirectiveBase):
-
-  def prepare(self, macro_name):
-    self.tokens = split_space_boundary(self.text)
-    if 5 <= len(self.tokens):
-      self.tokens[4] = macro_name
-      self.expected = ''.join(self.tokens)
-    else:
-      self.expected = F'{self.directive}  // {macro_name}'
+    def prepare(self, macro_name):
+        self.tokens = split_space_boundary(self.text)
+        if 5 <= len(self.tokens):
+            self.tokens[4] = macro_name
+            self.expected = "".join(self.tokens)
+        else:
+            self.expected = f"{self.directive}  // {macro_name}"
 
 
 class IncludeGuard:
+    def __init__(self):
+        self.ifndef = OpeningDirective("#ifndef")
+        self.define = OpeningDirective("#define")
+        self.endif = ClosingDirective("#endif")
+        self.pragma = False
 
-  def __init__(self):
-    self.ifndef = OpeningDirective('#ifndef')
-    self.define = OpeningDirective('#define')
-    self.endif  = ClosingDirective('#endif')
-    self.pragma = False
+    def items(self):
+        yield self.ifndef
+        yield self.define
+        yield self.endif
 
-  def items(self):
-    yield self.ifndef
-    yield self.define
-    yield self.endif
+    def is_none(self):
+        return any(item.is_none() for item in self.items())
 
-  def is_none(self):
-    return any(item.is_none() for item in self.items())
+    def mismatch(self):
+        return any(item.mismatch() for item in self.items())
 
-  def mismatch(self):
-    return any(item.mismatch() for item in self.items())
+    def overwrite(self, lines):
+        return any(item.overwrite(lines) for item in self.items())
 
-  def overwrite(self, lines):
-    return any(item.overwrite(lines) for item in self.items())
-
-  def prepare(self, macro_name):
-    for item in self.items(): item.prepare(macro_name)
+    def prepare(self, macro_name):
+        for item in self.items():
+            item.prepare(macro_name)
 
 
-def split_space_boundary(text, delimiters={' '}):
+def split_space_boundary(text, delimiters={" "}):
 
-  result = []
-  prev = None
-  for char in text:
-    curr = char in delimiters
-    if curr != prev:
-      result.append(char)
-    else:
-      result[-1] += char
-    prev = curr
-  return result
+    result = []
+    prev = None
+    for char in text:
+        curr = char in delimiters
+        if curr != prev:
+            result.append(char)
+        else:
+            result[-1] += char
+        prev = curr
+    return result
 
 
 def get_include_guard_info(lines):
 
-  guard = IncludeGuard()
-  for line, text in enumerate(lines):
-    if text.startswith('#pragma once'):
-      guard.pragma = True
-    if text.startswith('#ifndef') and guard.ifndef.is_none():
-      guard.ifndef.update(line, text)
-    if text.startswith('#define') and guard.define.is_none():
-      guard.define.update(line, text)
-    if text.startswith('#endif'):
-      guard.endif.update(line, text)
-  return guard
+    guard = IncludeGuard()
+    for line, text in enumerate(lines):
+        if text.startswith("#pragma once"):
+            guard.pragma = True
+        if text.startswith("#ifndef") and guard.ifndef.is_none():
+            guard.ifndef.update(line, text)
+        if text.startswith("#define") and guard.define.is_none():
+            guard.define.update(line, text)
+        if text.startswith("#endif"):
+            guard.endif.update(line, text)
+    return guard
 
 
 def get_parts_after(parts, targets):
-  result = []
-  for part in reversed(parts):
-    if part in targets:
-      break
-    result.append(part)
-  return reversed(result)
+    result = []
+    for part in reversed(parts):
+        if part in targets:
+            break
+        result.append(part)
+    return reversed(result)
 
 
 def get_include_guard_macro_name(filepath):
-  targets = {'include', 'src', 'test'}
-  parts = get_parts_after(filepath.parts, targets)
-  return '__'.join(parts).replace('.', '_').upper() + '_'
+    targets = {"include", "src", "test"}
+    parts = get_parts_after(filepath.parts, targets)
+    return "__".join(parts).replace(".", "_").upper() + "_"
 
 
 def main(argv=None):
 
-  parser = argparse.ArgumentParser()
-  parser.add_argument('filenames', nargs='*', help='Filenames to fix')
-  args = parser.parse_args(argv)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filenames", nargs="*", help="Filenames to fix")
+    args = parser.parse_args(argv)
 
-  return_code = 0
-  for filename in args.filenames:
-    # Search the include guard lines.
-    filepath = pathlib.Path(filename)
-    lines = filepath.read_text().split('\n')
-    guard = get_include_guard_info(lines)
-    # Skip if the file using pragma once.
-    if guard.pragma:
-      continue
-    # Error if the include guard is not found.
-    if guard.is_none():
-      print('No include guard in {}'.format(filepath))
-      return_code = 1
-      continue
-    # Error and auto fix if the macro name is not correct.
-    macro_name = get_include_guard_macro_name(filepath)
-    guard.prepare(macro_name)
-    if guard.mismatch():
-      print('Fix include guard in {}'.format(filepath))
-      return_code = 1
-      guard.overwrite(lines)
-      filepath.write_text('\n'.join(lines))
-      continue
+    return_code = 0
+    for filename in args.filenames:
+        # Search the include guard lines.
+        filepath = pathlib.Path(filename)
+        lines = filepath.read_text().split("\n")
+        guard = get_include_guard_info(lines)
+        # Skip if the file using pragma once.
+        if guard.pragma:
+            continue
+        # Error if the include guard is not found.
+        if guard.is_none():
+            print("No include guard in {}".format(filepath))
+            return_code = 1
+            continue
+        # Error and auto fix if the macro name is not correct.
+        macro_name = get_include_guard_macro_name(filepath)
+        guard.prepare(macro_name)
+        if guard.mismatch():
+            print("Fix include guard in {}".format(filepath))
+            return_code = 1
+            guard.overwrite(lines)
+            filepath.write_text("\n".join(lines))
+            continue
 
-  return return_code
+    return return_code
 
 
-if __name__ == '__main__':
-  exit(main())
+if __name__ == "__main__":
+    exit(main())
--- tests/test_ros_include_guard.py
+++ tests/test_ros_include_guard.py
@@ -1,37 +1,39 @@
-import pytest
-import shutil
 from pathlib import Path
+import shutil
+
+import pytest
+
 from pre_commit_hooks import ros_include_guard
 
 files = [
-  'include/rospkg/foobar.h',
-  'include/rospkg/foobar.hpp',
-  'include/rospkg/nolint.hpp',
-  'src/foo_bar_baz.hpp',
-  'src/foo_bar/baz.hpp',
-  'src/foo/bar_baz.hpp',
-  'src/foo/bar/baz.hpp',
+    "include/rospkg/foobar.h",
+    "include/rospkg/foobar.hpp",
+    "include/rospkg/nolint.hpp",
+    "src/foo_bar_baz.hpp",
+    "src/foo_bar/baz.hpp",
+    "src/foo/bar_baz.hpp",
+    "src/foo/bar/baz.hpp",
 ]
 
 cases = []
 for file in files:
-  cases.append((Path(file), 0))
-  cases.append((Path(file), 1))
+    cases.append((Path(file), 0))
+    cases.append((Path(file), 1))
 
 
-@pytest.mark.parametrize(('target_file', 'answer_code'), cases)
+@pytest.mark.parametrize(("target_file", "answer_code"), cases)
 def test(target_file, answer_code, datadir):
 
-  ok_ng = '.ng' if answer_code else '.ok'
-  source_file = target_file.with_suffix(ok_ng + target_file.suffix)
-  answer_file = target_file.with_suffix('.ok' + target_file.suffix)
-  target_path = datadir.joinpath(target_file)
-  source_path = datadir.joinpath(source_file)
-  answer_path = datadir.joinpath(answer_file)
-  shutil.copy(source_path, target_path)
+    ok_ng = ".ng" if answer_code else ".ok"
+    source_file = target_file.with_suffix(ok_ng + target_file.suffix)
+    answer_file = target_file.with_suffix(".ok" + target_file.suffix)
+    target_path = datadir.joinpath(target_file)
+    source_path = datadir.joinpath(source_file)
+    answer_path = datadir.joinpath(answer_file)
+    shutil.copy(source_path, target_path)
 
-  return_code = ros_include_guard.main([str(target_path)])
-  target_text = target_path.read_text()
-  answer_text = answer_path.read_text()
-  assert return_code == answer_code
-  assert target_text == answer_text
+    return_code = ros_include_guard.main([str(target_path)])
+    target_text = target_path.read_text()
+    answer_text = answer_path.read_text()
+    assert return_code == answer_code
+    assert target_text == answer_text
