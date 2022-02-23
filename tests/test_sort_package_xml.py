14f01a3593a9ff3bc3aba56da9b71e3657c2380d
Kenji Miyake <kenji.miyake@tier4.jp>
2022-02-23 19:17:22 +0900

    refactor
    Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>

--- tests/test_sort_package_xml.py
+++ tests/test_sort_package_xml.py
@@ -1,33 +1,33 @@
 from pathlib import Path
 import shutil
+from tempfile import NamedTemporaryFile
 
 import pytest
 
 from pre_commit_hooks import sort_package_xml
 
-files = [
-    "package.xml",
-]
 
-cases = []
-for file in files:
-    cases.append((Path(file), 0))
-    cases.append((Path(file), 1))
+@pytest.mark.parametrize(
+    "case",
+    [
+        "normal",
+        "no-format",
+    ],
+)
+def test(case: str, datadir: Path):
+    input_file = datadir / f"{case}.input.xml"
+    answer_file = datadir / f"{case}.answer.xml"
 
+    # Create a temporary file to prevent overwriting original files
+    tmp_file = Path(NamedTemporaryFile(mode="w", suffix=".xml").name)
+    shutil.copy(input_file, tmp_file)
 
-@pytest.mark.parametrize(("target_file", "answer_code"), cases)
-def test(target_file, answer_code, datadir):
+    # Format
+    return_code = sort_package_xml.main([str(tmp_file)])
+    print(tmp_file.read_text())
+    assert return_code == 1
+    assert tmp_file.read_text() == answer_file.read_text()
 
-    ok_ng = ".ng" if answer_code else ".ok"
-    source_file = target_file.with_suffix(ok_ng + target_file.suffix)
-    answer_file = target_file.with_suffix(".ok" + target_file.suffix)
-    target_path = datadir.joinpath(target_file)
-    source_path = datadir.joinpath(source_file)
-    answer_path = datadir.joinpath(answer_file)
-    shutil.copy(source_path, target_path)
-
-    return_code = sort_package_xml.main([str(target_path)])
-    target_text = target_path.read_text()
-    answer_text = answer_path.read_text()
-    assert return_code == answer_code
-    assert target_text == answer_text
+    # Re-format
+    return_code = sort_package_xml.main([str(tmp_file)])
+    assert return_code == 0
