d85f418beb01ed9ac6d8446edc64e98ed2dfdaa4
Kenji Miyake <31987104+kenji-miyake@users.noreply.github.com>
2022-02-24 14:51:34 +0900

    test(sort_package_xml): add tests (#35)
    * test(sort_package_xml): add tests
    
    Signed-off-by: Takagi, Isamu <isamu.takagi@tier4.jp>
    Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>
    
    * refactor
    
    Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>
    
    * add assert after re-format
    
    Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>
    
    Co-authored-by: Takagi, Isamu <isamu.takagi@tier4.jp>

---
+++ tests/test_sort_package_xml.py
@@ -0,0 +1,34 @@
+from pathlib import Path
+import shutil
+from tempfile import NamedTemporaryFile
+
+import pytest
+
+from pre_commit_hooks import sort_package_xml
+
+
+@pytest.mark.parametrize(
+    "case",
+    [
+        "normal",
+        "no-format",
+    ],
+)
+def test(case: str, datadir: Path):
+    input_file = datadir / f"{case}.input.xml"
+    answer_file = datadir / f"{case}.answer.xml"
+
+    # Create a temporary file to prevent overwriting original files
+    tmp_file = Path(NamedTemporaryFile(mode="w", suffix=".xml").name)
+    shutil.copy(input_file, tmp_file)
+
+    # Format
+    return_code = sort_package_xml.main([str(tmp_file)])
+    print(tmp_file.read_text())
+    assert return_code == 1
+    assert tmp_file.read_text() == answer_file.read_text()
+
+    # Re-format
+    return_code = sort_package_xml.main([str(tmp_file)])
+    assert return_code == 0
+    assert tmp_file.read_text() == answer_file.read_text()
