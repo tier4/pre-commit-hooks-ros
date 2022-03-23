f03d4da8823d6ffc03381ae990bd705914d4a5ef
Kenji Miyake <kenji.miyake@tier4.jp>
2022-03-23 12:42:55 +0900

    Revert "ci(pre-commit): autofix"
    This reverts commit 1846477affa6dd7843ac875e5af3bf60d119c241.

--- tests/test_ros_include_guard/include/rospkg/nolint.right.hpp
+++ tests/test_ros_include_guard/include/rospkg/nolint.right.hpp
@@ -1,3 +1,3 @@
 #ifndef ROSPKG__NOLINT_HPP_  // NOLINT
 #define ROSPKG__NOLINT_HPP_  // NOLINT
-#endif                       // ROSPKG__NOLINT_HPP_  // NOLINT
+#endif  // ROSPKG__NOLINT_HPP_  // NOLINT
