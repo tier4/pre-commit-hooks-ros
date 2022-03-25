87d11c46d4433b3529d826438c4f758bdd5821a1
Keisuke Shima <19993104+KeisukeShima@users.noreply.github.com>
2022-03-25 11:27:57 +0900

    chore(test): refactor test code
    Signed-off-by: Keisuke Shima <19993104+KeisukeShima@users.noreply.github.com>

--- tests/test_sort_package_xml.py
+++ tests/test_sort_package_xml.py
@@ -1,5 +1,4 @@
 from pathlib import Path
-import shutil
 
 import pytest
 
@@ -17,17 +16,13 @@ def test(case: str, datadir: Path, tmp_path: Path):
     input_file = datadir / f"{case}.input.xml"
     answer_file = datadir / f"{case}.answer.xml"
 
-    # Create a temporary file to prevent overwriting original files
-    tmp_file = tmp_path / f"{case}.input.xml"
-    shutil.copy(input_file, tmp_file)
-
     # Format
-    return_code = sort_package_xml.main([str(tmp_file)])
-    print(tmp_file.read_text())
+    return_code = sort_package_xml.main([str(input_file)])
+    print(input_file.read_text())
     assert return_code == 1
-    assert tmp_file.read_text() == answer_file.read_text()
+    assert input_file.read_text() == answer_file.read_text()
 
     # Re-format
-    return_code = sort_package_xml.main([str(tmp_file)])
+    return_code = sort_package_xml.main([str(input_file)])
     assert return_code == 0
-    assert tmp_file.read_text() == answer_file.read_text()
+    assert input_file.read_text() == answer_file.read_text()
